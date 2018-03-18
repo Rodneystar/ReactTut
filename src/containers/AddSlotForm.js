888c4058772d0a467dc32138fafffa80322179d8
Rodneystar <Rodneystar66@gmail.com>
2018-03-18 23:53:23 +0000

    second commit

--- src/containers/AddSlotForm.js
+++ src/containers/AddSlotForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import RunBackPane from './RunBackPane.js'
 
   class AddSlotForm extends React.Component {
     constructor(props) {
@@ -21,32 +22,36 @@ import axios from 'axios';
 
 	render() {
 		return(
-      <div className="container container-blue form form-left">
-    	 <header>
-        	<h5>Add timer slot:</h5>
-       </header>
-	       <span>
+      <div className="container container-blue">
+        <header>
+           <h5>Add timer slot:</h5>
+        </header>
 
-            <label htmlFor="startTime">Time to start:</label>
-            <input id="startTime" type="time" name="startTime"  value={this.state.start}
-							onChange={(event) => {
-								this.setState({start: event.target.value})
-							}}/>
+  	       <span className="form form-left" id="addForm">
 
-					<label htmlFor="length">length (minutes): </label>
-            <input id="length" type="number" name="length" min="30" max="1490" step="30"  value={this.state.length}
-							onChange={(event) =>{
-								this.setState({length: event.target.value});
-							}}/>
+              <label htmlFor="startTime">Time to start:</label>
+              <input id="startTime" type="time" name="startTime"  value={this.state.start}
+  							onChange={(event) => {
+  								this.setState({start: event.target.value})
+  							}}/>
 
-					<div className="buttonSpacingTop">
-        		<input id="addButton" type="button" value="Add slot" onClick={this.handleAdd}></input>
-					</div>
+  					<label htmlFor="length">length (minutes): </label>
+              <input id="length" type="number" name="length" min="30" max="1490" step="30"  value={this.state.length}
+  							onChange={(event) =>{
+  								this.setState({length: event.target.value});
+  							}}/>
 
-	       </span>
-       <footer>
-			 </footer>
-    	</div>
+  					<div className="buttonSpacingTop">
+          		<input id="addButton" type="button" value="Add slot" onClick={this.handleAdd}></input>
+  					</div>
+
+  	       </span>
+
+
+        <RunBackPane runBackFinish={this.props.runBackFinish}/>
+        <footer>
+        </footer>
+      </div>
 		)
 	}
 }
